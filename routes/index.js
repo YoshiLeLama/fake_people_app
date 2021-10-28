74f089f3a65c53c96d762425c04f7e41c8fe4096
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2021-10-28 18:41:30 +0200

    renamed generateCats method

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-const { generateAnimals: generateCats } = require('../controllers/generate_animals');
+const { generateAnimals } = require('../controllers/generate_animals');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const animals = {animals: generateCats(10)};
+  const animals = {animals: generateAnimals(10)};
   console.log(animals);
   res.render('index', animals);
 });
